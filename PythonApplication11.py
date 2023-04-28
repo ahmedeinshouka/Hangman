b62a7880614b0f4bcce5d8eddace2ca799ea9a01
Ahmed Einshouka <76920911+ahmedeinshouka@users.noreply.github.com>
2023-04-28 09:44:39 +0200

    Add files via upload

---
+++ PythonApplication11.py
@@ -0,0 +1,385 @@
+
+
+
+
+import random
+stages = ['''
+
+  
+
+     +---+
+
+     |   |
+
+         |
+
+         |
+
+         |
+
+         |
+
+  =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+        |
+
+        |
+
+        |
+
+  =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+    |   |
+
+        |
+
+        |
+
+  =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+   /|   |
+
+        |
+
+        |
+
+  =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+   /|\  |
+
+        |
+
+        |
+
+   =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+   /|\  |
+
+   /    |
+
+        |
+
+  =========''', '''
+
+ 
+
+    +---+
+
+    |   |
+
+    O   |
+
+   /|\  |
+
+   / \  |
+
+        |
+
+  =========''']
+print("                welcome to  Hangman Game                       ")
+print("************************************************************************************\n")
+print(''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    \n\n''')
+print("**************************************************************************************")
+word_list= [
+'abruptly', 
+'absurd', 
+'abyss', 
+'affix', 
+'askew', 
+'avenue', 
+'awkward', 
+'axiom', 
+'azure', 
+'bagpipes', 
+'bandwagon', 
+'banjo', 
+'bayou', 
+'beekeeper', 
+'bikini', 
+'blitz', 
+'blizzard', 
+'boggle', 
+'bookworm', 
+'boxcar', 
+'boxful', 
+'buckaroo', 
+'buffalo', 
+'buffoon', 
+'buxom', 
+'buzzard', 
+'buzzing', 
+'buzzwords', 
+'caliph', 
+'cobweb', 
+'cockiness', 
+'croquet', 
+'crypt', 
+'curacao', 
+'cycle', 
+'daiquiri', 
+'dirndl', 
+'disavow', 
+'dizzying', 
+'duplex', 
+'dwarves', 
+'embezzle', 
+'equip', 
+'espionage', 
+'euouae', 
+'exodus', 
+'faking', 
+'fishhook', 
+'fixable', 
+'fjord', 
+'flapjack', 
+'flopping', 
+'fluffiness', 
+'flyby', 
+'foxglove', 
+'frazzled', 
+'frizzled', 
+'fuchsia', 
+'funny', 
+'gabby', 
+'galaxy', 
+'galvanize', 
+'gazebo', 
+'giaour', 
+'gizmo', 
+'glowworm', 
+'glyph', 
+'gnarly', 
+'gnostic', 
+'gossip', 
+'grogginess', 
+'haiku', 
+'haphazard', 
+'hyphen', 
+'iatrogenic', 
+'icebox', 
+'injury', 
+'ivory', 
+'ivy', 
+'jackpot', 
+'jaundice', 
+'jawbreaker', 
+'jaywalk', 
+'jazziest', 
+'jazzy', 
+'jelly', 
+'jigsaw', 
+'jinx', 
+'jiujitsu', 
+'jockey', 
+'jogging', 
+'joking', 
+'jovial', 
+'joyful', 
+'juicy', 
+'jukebox', 
+'jumbo', 
+'kayak', 
+'kazoo', 
+'keyhole', 
+'khaki', 
+'kilobyte', 
+'kiosk', 
+'kitsch', 
+'kiwifruit', 
+'klutz', 
+'knapsack', 
+'larynx', 
+'lengths', 
+'lucky', 
+'luxury', 
+'lymph', 
+'marquis', 
+'matrix', 
+'megahertz', 
+'microwave', 
+'mnemonic', 
+'mystify', 
+'naphtha', 
+'nightclub', 
+'nowadays', 
+'numbskull', 
+'nymph', 
+'onyx', 
+'ovary', 
+'oxidize', 
+'oxygen', 
+'pajama', 
+'peekaboo', 
+'phlegm', 
+'pixel', 
+'pizazz', 
+'pneumonia', 
+'polka', 
+'pshaw', 
+'psyche', 
+'puppy', 
+'puzzling', 
+'quartz', 
+'queue', 
+'quips', 
+'quixotic', 
+'quiz', 
+'quizzes', 
+'quorum', 
+'razzmatazz', 
+'rhubarb', 
+'rhythm', 
+'rickshaw', 
+'schnapps', 
+'scratch', 
+'shiv', 
+'snazzy', 
+'sphinx', 
+'spritz', 
+'squawk', 
+'staff', 
+'strength', 
+'strengths', 
+'stretch', 
+'stronghold', 
+'stymied', 
+'subway', 
+'swivel', 
+'syndrome', 
+'thriftless', 
+'thumbscrew', 
+'topaz', 
+'transcript', 
+'transgress', 
+'transplant', 
+'triphthong', 
+'twelfth', 
+'twelfths', 
+'unknown', 
+'unworthy', 
+'unzip', 
+'uptown', 
+'vaporize', 
+'vixen', 
+'vodka', 
+'voodoo', 
+'vortex', 
+'voyeurism', 
+'walkway', 
+'waltz', 
+'wave', 
+'wavy', 
+'waxy', 
+'wellspring', 
+'wheezy', 
+'whiskey', 
+'whizzing', 
+'whomever', 
+'wimpy', 
+'witchcraft', 
+'wizard', 
+'woozy', 
+'wristwatch', 
+'wyvern', 
+'xylophone', 
+'yachtsman', 
+'yippee', 
+'yoked', 
+'youthful', 
+'yummy', 
+'zephyr', 
+'zigzag', 
+'zigzagging', 
+'zilch', 
+'zipper', 
+'zodiac', 
+'zombie', 
+]
+rand_word=random.choice(word_list)
+blanks=["_"]*len(rand_word)
+blanks2=str(blanks)
+print("\n")
+print(blanks2)
+print("\n")
+print("\n***********************************************************************************\n")
+print(" welcome our super hero please start to guess each letter")
+print("\n***********************************************************************************\n")
+print(rand_word)
+end_game=False
+lives=7
+while not end_game :
+            
+            guess=input("please enter the guess letter: ").lower()  
+            for position in range(len(rand_word)) :
+                         
+                         letter=rand_word[position]
+                   
+                         if guess==str(letter):
+                            blanks[position]=letter
+            if guess not in rand_word:
+                lives-=1
+                if lives==0:
+                    end_game=True
+                    print("You lose")    
+            print("*************************************************************************\n")
+            print(f"{blanks}\n\n\n")            
+            if "_" not in blanks :
+             end_game=True
+             print("you win")
+             print("*************************************************************************")
+            print(f"\n You guessed {guess},that's not in the word,you lose a life")
+            print(stages[-lives-1]) 
+            print("**************************************************************************")
+            if lives==0:
+                print("\n you lose")
+                print("\n**************************************************************************")
+                    
+    
